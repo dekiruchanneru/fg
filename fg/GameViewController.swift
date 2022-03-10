861247f8f3d744e0b9e2eef2d043932f5083b419
dekiruchanneru <87129570+dekiruchanneru@users.noreply.github.com>
2022-03-10 23:14:01 +0900

    Initial Commit

---
+++ fg/GameViewController.swift
@@ -0,0 +1,50 @@
+//
+//  GameViewController.swift
+//  fg
+//
+//  Created by Ramadhan Saputra on 2022/03/10.
+//  Copyright © 2022 Ramadhan Saputra. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
